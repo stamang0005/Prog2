3bee551a11f4b12cc5ec5305608c475fe149d67d
stamang0005 <63462456+stamang0005@users.noreply.github.com>
2020-04-15 20:34:32 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Prog2
+{
+    partial class Program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputLNameInitial = new System.Windows.Forms.TextBox();
+            this.RseniorButton = new System.Windows.Forms.RadioButton();
+            this.Rjuniorbutton = new System.Windows.Forms.RadioButton();
+            this.RsophomoreButton = new System.Windows.Forms.RadioButton();
+            this.RfreshmanButton = new System.Windows.Forms.RadioButton();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Last Name Initial:";
+            // 
+            // InputLNameInitial
+            // 
+            this.InputLNameInitial.Location = new System.Drawing.Point(140, 58);
+            this.InputLNameInitial.Name = "InputLNameInitial";
+            this.InputLNameInitial.Size = new System.Drawing.Size(100, 20);
+            this.InputLNameInitial.TabIndex = 1;
+            // 
+            // RseniorButton
+            // 
+            this.RseniorButton.AutoSize = true;
+            this.RseniorButton.Location = new System.Drawing.Point(52, 96);
+            this.RseniorButton.Name = "RseniorButton";
+            this.RseniorButton.Size = new System.Drawing.Size(55, 17);
+            this.RseniorButton.TabIndex = 2;
+            this.RseniorButton.TabStop = true;
+            this.RseniorButton.Text = "Senior";
+            this.RseniorButton.UseVisualStyleBackColor = true;
+            // 
+            // Rjuniorbutton
+            // 
+            this.Rjuniorbutton.AutoSize = true;
+            this.Rjuniorbutton.Location = new System.Drawing.Point(52, 119);
+            this.Rjuniorbutton.Name = "Rjuniorbutton";
+            this.Rjuniorbutton.Size = new System.Drawing.Size(53, 17);
+            this.Rjuniorbutton.TabIndex = 3;
+            this.Rjuniorbutton.TabStop = true;
+            this.Rjuniorbutton.Text = "Junior";
+            this.Rjuniorbutton.UseVisualStyleBackColor = true;
+            // 
+            // RsophomoreButton
+            // 
+            this.RsophomoreButton.AutoSize = true;
+            this.RsophomoreButton.Location = new System.Drawing.Point(52, 143);
+            this.RsophomoreButton.Name = "RsophomoreButton";
+            this.RsophomoreButton.Size = new System.Drawing.Size(79, 17);
+            this.RsophomoreButton.TabIndex = 4;
+            this.RsophomoreButton.TabStop = true;
+            this.RsophomoreButton.Text = "Sophomore";
+            this.RsophomoreButton.UseVisualStyleBackColor = true;
+            // 
+            // RfreshmanButton
+            // 
+            this.RfreshmanButton.AutoSize = true;
+            this.RfreshmanButton.Location = new System.Drawing.Point(52, 167);
+            this.RfreshmanButton.Name = "RfreshmanButton";
+            this.RfreshmanButton.Size = new System.Drawing.Size(71, 17);
+            this.RfreshmanButton.TabIndex = 5;
+            this.RfreshmanButton.TabStop = true;
+            this.RfreshmanButton.Text = "Freshman";
+            this.RfreshmanButton.UseVisualStyleBackColor = true;
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(52, 209);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(75, 23);
+            this.ExecuteButton.TabIndex = 6;
+            this.ExecuteButton.Text = "Show";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // Program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.RfreshmanButton);
+            this.Controls.Add(this.RsophomoreButton);
+            this.Controls.Add(this.Rjuniorbutton);
+            this.Controls.Add(this.RseniorButton);
+            this.Controls.Add(this.InputLNameInitial);
+            this.Controls.Add(this.label1);
+            this.Name = "Program2";
+            this.Text = "Program2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputLNameInitial;
+        private System.Windows.Forms.RadioButton RseniorButton;
+        private System.Windows.Forms.RadioButton Rjuniorbutton;
+        private System.Windows.Forms.RadioButton RsophomoreButton;
+        private System.Windows.Forms.RadioButton RfreshmanButton;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿//B8030
+//Cis 199
+//Program 2
+// Prof. Andrew
+// due 10/18/2018
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Program2 : Form
+    {
+        public Program2()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            char letter;// seting letter variable
+            string userInput = InputLNameInitial.Text;// putting input for user to put
+            letter = userInput[0];// selects the first letter of the lastname entered.
+            
+
+            char.IsLetter(letter);//letting the program know its a letter
+
+            {
+                letter = char.ToUpper(letter);// with this program accept any letter, wheater upper case or lower case
+
+
+                if (RseniorButton.Checked)// if statment excecute when senior is checked
+                { 
+                    if (letter <= 'D')//logic if the condition then run the messgage box.
+                        MessageBox.Show("Rigester opens on: 4:00 PM, FRIDAY NOV. 2");// output if the condition is meet
+                    else if (letter <= 'I')//logic if the condition then run the messgage box.
+                        MessageBox.Show("Rigester opens on: 8:30 AM, FRIDAY NOV. 2");// output if the condition is meet
+                    else if (letter <= 'O')//logic if the condition then run the messgage box.
+                        MessageBox.Show("Rigester opens on: 10:00 AM, FRIDAY NOV. 2");// output if the condition is meet
+                    else if (letter <= 'S')//logic if the condition then run the messgage box.
+                        MessageBox.Show("Rigester opens on: 11:30 AM, FRIDAY NOV. 2");// output if the condition is meet
+                    else
+                        MessageBox.Show("Rigester opens on: 2:00 PM FRIDAY NOV. 2");// output if the condition is meet
+                }
+
+            if (Rjuniorbutton.Checked)//if statement that exceute when junior is checked
+            {// if statement if then conditon is meet then it display the result underneath other wise it exceute the else if conditoin until it find the ture condtion.
+                if (letter <= 'D')
+                    MessageBox.Show("Rigester opens on: 4:00, MONDAY, NOV. 5");
+                else if (letter <= 'I')
+                    MessageBox.Show("Rigester opens on: 8:30, MONDAY, NOV. 5");
+                else if (letter <= 'O')
+                    MessageBox.Show("Rigester opens on: 10:00, MONDAY, NOV. 5");
+                else if (letter <= 'S')
+                    MessageBox.Show("Rigester opens on: 11:30, MONDAY, NOV. 5");
+                else
+                    MessageBox.Show("Rigester opens on: 2:00, MONDAY, NOV. 5");
+            }
+        
+
+                if (RsophomoreButton.Checked)// if statement that excecute when Sophomore is checked in the radio button.
+                {// if statement if then conditon is meet then it display the result underneath other wise it exceute the else if conditoin until it find the ture condtion.
+                    if (letter <= 'B')
+                        MessageBox.Show("Rigester opens on: 2:00, WEDNESDAY, NOV. 7");
+                    else if (letter <= 'D')
+                        MessageBox.Show("Rigester opens on: 4:00, WEDNESDAY, NOV. 7");
+                    else if (letter <= 'F')
+                        MessageBox.Show("Rigester opens on: 8:30, TUESDAY, NOV. 6");
+                    else if (letter <= 'I')
+                        MessageBox.Show("Rigester opens on: 10:00, TUESDAY, NOV. 6");
+                    else if (letter <= 'L')
+                        MessageBox.Show("Rigester opens on: 11:30, TUESDAY, NOV. 6");
+                    else if (letter <= 'O')
+                        MessageBox.Show("Rigester opens on: 2:00, TUESDAY, NOV. 6");
+                    else if (letter <= 'Q')
+                        MessageBox.Show("Rigester opens on: 4:00, TUESDAY, NOV. 6");
+                    else if (letter <= 'S')
+                        MessageBox.Show("v 8:30, WEDNESDAY, NOV. 7");
+                    else if (letter<= 'V')
+                        MessageBox.Show("Rigester opens on: 10:00, WEDNESDAY, NOV. 7");
+                    else MessageBox.Show("Rigester opens on: 11:30, WEDNESDAY, NOV. 7");
+
+
+                }
+                if (RfreshmanButton.Checked)// if statement that excecute when Sophomore is checked in the radio button.
+                {// if statement if then conditon is meet then it display the result underneath other wise it exceute the else if conditoin until it find the ture condtion.
+                    if (letter <= 'B')
+                        MessageBox.Show("Rigester opens on: 2:00, FRIDAY, NOV. 9");
+                    else if (letter <= 'D')
+                        MessageBox.Show("Rigester opens on: 4:00, FRIDAY, NOV. 9");
+                    else if (letter <= 'F')
+                        MessageBox.Show("Rigester opens on: 8:30, THURSDAY, NOV. 8");
+                    else if (letter <= 'I')
+                        MessageBox.Show("Rigester opens on: 10:00, THURSDAY, NOV. 8");
+                    else if (letter <= 'L')
+                        MessageBox.Show("Rigester opens on: 11:30, THURSDAY, NOV. 8");
+                    else if (letter <= 'O')
+                        MessageBox.Show("Rigester opens on: 2:00, THURSDAY, NOV. 8");
+                    else if (letter <= 'Q')
+                        MessageBox.Show("Rigester opens on: 4:00, THURSDAY, NOV. 8");
+                    else if (letter <= 'S')
+                        MessageBox.Show("Rigester opens on: 8:30, FRIDAY, NOV. 9");
+                    else if (letter <= 'V')
+                        MessageBox.Show("Rigester opens on: 10:00,FRIDAY, NOV. 9");
+                    else MessageBox.Show("Rigester opens on: 11:30, FRIDAY, NOV. 9");
+
+                }
+
+
+
+
+
+
+
+
+
+                }
+                
+            }
+    }
+}
